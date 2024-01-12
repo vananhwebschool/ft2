cbe951f8e3ee1d16791b3e7118b07bf654681a8f
tvanh4 <tvanh4@myboost.co>
2024-01-12 23:04:46 +0100

    donnees generales component en bas a gauche 1er page

--- dataviz/src/App.jsx
+++ dataviz/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Component1 from './resumeAnnee/component1'
-import Map from "./Map"
+import DataBox from "./component-bas-gauche/DataBox.jsx";
+import CommonBox from "./component-bas-gauche/CommonBox.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Component1></Component1>
-      <Map/>
+    <DataBox boxNumber={1}>
+      <CommonBox />
+    </DataBox>
+    <DataBox boxNumber={2}>
+      <CommonBox />
+    </DataBox>
+    <DataBox boxNumber={3}>
+      <CommonBox />
+    </DataBox>
     </>
   )
 }
---
+++ dataviz/src/component-bas-gauche/context/BoxNumberContext.jsx
@@ -0,0 +1,3 @@
+import { createContext } from 'react';
+
+export const BoxNumberContext = createContext(1);
