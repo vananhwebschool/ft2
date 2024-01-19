eb0a014ceac4e65da080e2604f651d34ba079679
tvanh4 <tvanh4@myboost.co>
2024-01-19 14:00:23 +0100

    remove premier-page-component de Anh's page - to merge with Leane's component

--- dataviz/src/main.jsx
+++ dataviz/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import SecondPage from "./second-page-line-chart/SecondPage.jsx";
-import FirstPage from "./premiere-page-carte/FirstPage.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- dataviz/src/premiere-page-carte/component-bas-gauche/context/BoxNumberContext.jsx
+++
@@ -1,3 +0,0 @@
-import { createContext } from 'react';
-
-export const BoxNumberContext = createContext(1);
